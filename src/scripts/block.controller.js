ac8fa999c019cba729914deda7839d95e2c59b7d
Kdehner <Kdehner@gmail.com>
2019-12-17 20:09:00 -0700

    logic for adding tiles to containing block

--- src/scripts/block.controller.js
+++ src/scripts/block.controller.js
@@ -3,8 +3,8 @@ export class Block {
   constructor(id, game) {
     this.id = id;
     this.element = this.createElement();
+    this.tile;
     this.injectDom(game);
-    this.adjacent = getAdjacent();
   }
 
   createElement() {
@@ -14,12 +14,24 @@ export class Block {
     return div;
   }
 
+  set setTile(tile) { 
+    this.tile = tile;
+    this.appendChild(tile);
+  }
+
+  removeChild() {
+    const child = this.tile.getDomElement;
+    this.element.removeChild(child);
+    this.tile = null;
+  }
+
   injectDom(game) {
     game.appendChild(this.element);
   }
 
-  getAdjacent() {
-    // Determin adjacent blocks in each direction.
+  appendChild (tile) {
+    const tileElement = tile.getDomElement;
+    this.element.appendChild(tileElement);
   }
 
 }
